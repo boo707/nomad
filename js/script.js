c3c084d101634023fe5d25f063c16f6e5bad1783
boo707 <buuu0708@gmail.com>
2024-07-24 16:20:18 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//tab
+function openBoard(evt, boardName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("board");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" opacity", "");
+    }
+    document.getElementById(boardName).style.display = "block";
+    evt.currentTarget.className += " opacity";
+}
+
+// toggle
+$(function(){
+    $(".faq> li> .questions_box").click(function(){
+        $(this).next().slideToggle();
+        $('.q_icon p').toggleClass('qa')
+    })
+})
+$(function(){
+    $(".faq> li> ul").click(function(){
+        $(this).children().children("img").toggleClass("turn")
+    })
+})
+
+
