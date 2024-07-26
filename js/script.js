2a3bfbab4912ec7ff6d092b10499432d311476dd
boo707 <buuu0708@gmail.com>
2024-07-26 17:07:00 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,4 +26,19 @@ $(function(){
     })
 })
 
+//tab_trip
+function openBoard(evt, boardName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("theme_content");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" opacity", "");
+    }
+    document.getElementById(boardName).style.display = "block";
+    evt.currentTarget.className += " opacity";
+}
+
 
